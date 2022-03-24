ceea56e38bc04feaf2cab5c1f6e081bbfa6f62bd
Kai Schneider <schneikai@gmail.com>
2022-03-24 12:29:40 +0100

    Add Bookmarks scaffold to have something to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :bookmarks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'bookmarks#index'
 end
