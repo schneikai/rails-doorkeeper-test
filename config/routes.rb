c11d0e84618af887f29d9f8f6476aa200f9c5d84
schneikai <schneikai@gmail.com>
2022-03-28 17:30:08 +0200

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users
   resources :bookmarks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'bookmarks#index'
+  root 'home#index'
 end
